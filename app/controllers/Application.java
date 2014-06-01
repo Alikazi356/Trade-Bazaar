a38e086948ed9b20a7bb8dc65e9c29c417868284
Ali Kazi <alimuhammadkazi@yahoo.com>
2014-06-02 01:08:33 +0200

    template & graph code...

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,19 +1,15 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
 
+import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-    	   System.out.print("aliu"); 
-    	   
-    	   System.out.print("aeyy rajoooooooooooooooooooooooooooooooooooooooooooooo"); 
-         return ok(index.render("Sajjad Ali 123"));
-         
-     
+    public static Result index(){
+        return ok(index.render());
     }
-
+   
 }
+
+
--- build.sbt
+++ build.sbt
@@ -3,9 +3,12 @@ name := "Trade-Bazaar"
 version := "1.0-SNAPSHOT"
 
 libraryDependencies ++= Seq(
+ "mysql" % "mysql-connector-java" % "5.1.21",
   javaJdbc,
   javaEbean,
   cache
 )     
 
 play.Project.playJavaSettings
+
+play.Project.playScalaSettings
